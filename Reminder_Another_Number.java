da344523306ca6ed08c7af111159f520722dd72d
Somen Pradhan <somenpradhan135@gmail.com>
2025-03-12 20:38:34 +0530

    Add files via upload

---
+++ Reminder_Another_Number.java
@@ -0,0 +1,12 @@
+/*Write a program to find the reminder of a number when divided by another number.*/
+class Sample{
+	public static void main(String[] args)
+    {
+		int dividend  = 21;
+        int divisor = 2;
+		
+        int reminder = dividend % divisor;
+
+		System.out.println(" The Reminder is :" + reminder);
+	}
+}
